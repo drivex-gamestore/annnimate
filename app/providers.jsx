6fca2563cab7e165dfd46264806c0f6e575e13f9
drivex store <300916779+drivex-gamestore@users.noreply.github.com>
2026-09-05 10:36:20 +0630

    update structure

--- app/providers.jsx
+++ app/providers.jsx
@@ -4,21 +4,32 @@ import LenisProvider from '@providers/LenisProvider'
 import TransitionRouterContext, { RouterTransition } from '@providers/TransitionRouterProvider'
 import AnimationProvider from '@providers/AnimationProvider'
 import CategoryProvider from '@providers/CategoryProvider'
+import CookieConsentProvider from '@providers/CookieConsentProvider'
+import { UserProvider } from '@providers/UserProvider'
+import { ScrollLockProvider } from '@providers/ScrollLockProvider'
+import AppProvidersExtras from '@providers/AppProviders'
 
 const noop = async () => {};
 
 export default function AppProviders({ children }) {
   return (
-    <LenisProvider>
-      <RouterTransition leave={noop} enter={noop}>
-        <TransitionRouterContext>
-          <AnimationProvider>
-            <CategoryProvider>
-              {children}
-            </CategoryProvider>
-          </AnimationProvider>
-        </TransitionRouterContext>
-      </RouterTransition>
-    </LenisProvider>
+    <UserProvider>
+      <ScrollLockProvider>
+        <LenisProvider>
+          <RouterTransition leave={noop} enter={noop}>
+            <TransitionRouterContext>
+              <AnimationProvider>
+                <CategoryProvider>
+                  <AppProvidersExtras>
+                    {children}
+                  </AppProvidersExtras>
+                  <CookieConsentProvider />
+                </CategoryProvider>
+              </AnimationProvider>
+            </TransitionRouterContext>
+          </RouterTransition>
+        </LenisProvider>
+      </ScrollLockProvider>
+    </UserProvider>
   );
 }
--- src/features/applayout.jsx
+++ src/features/applayout.jsx
@@ -1,9 +1,9 @@
-import Loader from '@animations/components/Loader';
+import PreloaderWrapper from '@animations/utils/PreloaderWrapper';
 import Header from '@features/layout/header/Header';
 export default async function AppLayout({ children }) {
   return (
     <>
-      <Loader />
+      <PreloaderWrapper />
       <Header />
         <main class="flex-1 relative z-[2] bg-background" data-transition-content="true">
           {children}
