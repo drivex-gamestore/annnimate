4b85383537a48133266b8de5f60cfe8beb3211de
drivex store <300916779+drivex-gamestore@users.noreply.github.com>
2026-09-05 14:05:05 +0630

    fixed CookiePreferencesModal

--- src/features/components/ui/Dialog.jsx
+++ src/features/components/ui/Dialog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import { cn } from '@lib/vendor';
 import { useScrollLockContext } from '@providers/ScrollLockProvider';
 
 function DialogPortal({ ...props }) {
